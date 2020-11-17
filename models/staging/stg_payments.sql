987d45ad34dd5bd92fc1899a70d4bfb20a23604e
Maryanne Li <maryanne406@gmail.com>
2020-11-17 17:00:38 +0000

    create stg_payment table

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,6 +1,6 @@
-select 
+SELECT 
     orderid as order_id,
-    amount
-from raw.stripe.payment
-WHERE
-    status = 'success'
+    status as order_status,
+    amount as payment_amount
+FROM raw.stripe.payment
+
